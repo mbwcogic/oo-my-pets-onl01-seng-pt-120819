dc41fb6799a324a187e0d8b5a6a488db27a64470
Michael Waldo <mbwcogic@yahoo.com>
2020-01-25 23:25:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,24 @@
 class Owner
   
   attr_reader :name, :species
+  
+  @@all = []
+ 
   def initialize(name) 
     @name = name
     @species = "human"
+    save 
   end 
+  
   def say_species 
   "I am a #{@species}."  
+  end 
+  
+  def save 
+   @@all << self 
+  end
+  
+  def self.all
+    @@all
   end 
 end
